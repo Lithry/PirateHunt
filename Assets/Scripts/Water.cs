01ca4edd0b7dc0a3bdd869d52e34b9e1ae9696d2
Lithry <facundosargiotti@gmail.com>
2017-09-23 21:16:10 -0300

    Agua animada

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+	public float scrollSpeed = 0.5F;
+	private bool isInside;
+	private RectTransform rtra;
+	private RectTransform pRtra;
+	private Vector3 localSpacePoint;
+	private Vector3[] corners = new Vector3[4];
+    
+	void Start() {
+		rtra = GetComponent<RectTransform>();
+		pRtra = GetComponentInParent<RectTransform>();
+		rtra.GetWorldCorners(corners);
+    }
+    
+	void LateUpdate() {
+		if (transform.position.y <= 1)
+			transform.position = new Vector3(transform.position.x, Screen.height * 2 - 8, transform.position.z);
+		
+		transform.position = new Vector3(transform.position.x, transform.position.y - scrollSpeed * Time.deltaTime, transform.position.z);
+  	}
+}
